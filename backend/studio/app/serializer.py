b042ac953e1100694eea3141cfe84f571fa42eaa
portyanq <portyan19@gmail.com>
2021-07-13 01:27:04 +0300

    try

--- backend/studio/app/serializer.py
+++ backend/studio/app/serializer.py
@@ -1,6 +1,26 @@
 from django.contrib.auth.models import User
-from .models import (Case, Ready, Review, Report, PersReady, FormData)
+from .models import *
 from rest_framework import serializers
 
+
+class CaseModelSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Case
+        fields = ('id', 'name', 'url', 'img')
+
+
+class CaseHyperlinkedModelSerializer(serializers.HyperlinkedModelSerializer):
+    
+    class Meta:
+        model = Case
+        fields = ('id', 'name', 'url', 'img')
+
+
+class CaseListSerializer(serializers.ListSerializer):
+
+    class Meta:
+        model = Case
+        fields = ('id', 'name', 'url', 'img')
 
 
