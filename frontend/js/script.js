f776b9522b561554dd6dd1542e201f88cdeaa431
nikitaamikhailov <60756210+nikitaamikhailov@users.noreply.github.com>
2021-07-15 21:09:54 +0300

    minimal update

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,1 +1,7 @@
-console.log('sus')
+const swiper1 = new Swiper('.swiper-container', {
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
