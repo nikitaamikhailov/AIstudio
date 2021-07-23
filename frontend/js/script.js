dd7202ad6318d44ced07bfce91c0a3af74bc18c8
nikitaamikhailov <60756210+nikitaamikhailov@users.noreply.github.com>
2021-07-23 14:13:50 +0300

    minimal update

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,7 +1,23 @@
-const swiper1 = new Swiper('.swiper-container', {
+const swiper1 = new Swiper('.main-info_slider', {
     loop: true,
+    simulateTouch: true,
+    spaceBetween: 100,
+    pagination: {
+        el: '.main-swiper-pagination',
+        clickable: true
+    },
+})
+
+const swiperReviews = new Swiper('.reviews_cards', {
+    loop: true,
+    spaceBetween: 75,
+    pagination: {
+        el: '.reviews-swiper-pagination',
+        clickable: true
+    },
+    slidesPerView: 3,
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.reviews-swiper-button-next',
+        prevEl: '.reviews-swiper-button-prev',
     },
 })
