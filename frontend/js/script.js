7d9359f7e64172ef44a38e8395a787fec87afba9
nikitaamikhailov <60756210+nikitaamikhailov@users.noreply.github.com>
2021-08-09 16:14:35 +0300

    +minimal updates

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -48,6 +48,10 @@ const personalAreaButtons = document.querySelectorAll('.personal-area-button')
 const hiddenElements = document.querySelectorAll('.can-be-hidden')
 const personalArea = document.querySelector('.personal-area-form')
 const logo = document.querySelector('.logo')
+const firstAnswer = document.querySelectorAll('.first-question_cards_card')
+const firstQuestion = document.querySelector('.first-question')
+const secondQuestion = document.querySelector('.second-question')
+const secondAnswer = document.querySelectorAll('.second-question_cards_card')
 
 for (let personalAreaButton of personalAreaButtons){
     personalAreaButton.addEventListener("click", function (event) {
@@ -66,3 +70,16 @@ logo.addEventListener("click", function (event) {
     personalArea.classList.add('hidden')
 })
 
+for (let answer of firstAnswer){
+    answer.addEventListener("click", function (event) {
+        event.preventDefault()
+        firstQuestion.classList.add('hidden')
+        secondQuestion.classList.remove('hidden')
+    })}
+
+for (let answer of secondAnswer){
+    answer.addEventListener("click", function (event) {
+        event.preventDefault()
+        secondQuestion.classList.add('hidden')
+        firstQuestion.classList.remove('hidden')
+    })}
